1e5f166f9680b477668e2d80dbb112a50d86d758
rellons <yeksa.ningrat@gmail.com>
2016-09-27 11:56:53 +0700

    Added HC.H & HC.cpp

---
+++ src/HC.cpp
@@ -0,0 +1,28 @@
+#include "HC.h"
+
+HillClimb::HillClimb(SchedCSP csp):csp(csp){};
+
+SchedCSP* HillClimb::getSolution(){
+	csp.initAllVars();
+	int idxConflict = csp.findFirstConflicts();
+	int conflict	= csp.countConflicts();
+	int newConflict ;
+	int counter = 0;
+	while((conflict>0)&&(counter<1000)){
+		csp.initSingleVar(idxConflict);	
+		newConflict = csp.countConflicts();
+		if(newConflict < conflict){
+			idxConflict = csp.findFirstConflicts();
+			conflict = newConflict;
+			counter = 0;
+		}
+		else{
+			counter++;
+		}
+		cout<<csp.countConflicts()<<endl;
+	}
+
+	return &csp;
+	
+	
+};
